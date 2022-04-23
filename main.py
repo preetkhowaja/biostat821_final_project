3c4a9043459206f16fc5f47e0622577f585fd645
Preet Khowaja <preet.khowaja@duke.edu>
2022-04-23 18:57:44 -0400

    added main file

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+import uvicorn
+import math
+from imdb import IMDb
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello folks!"}
+
+
+@app.get("/movie/string1")
+async def movie(string1: str):
+    ia = IMDb()
+    name = string1
+    emp_list = []
+    search = ia.search_movie(name)
+    for i in search:
+        emp_list.append(i["title"])
+    return emp_list
+
+
+@app.get("/genre/string2")
+async def genre(string2: str):
+    ia = IMDb()
+    info = ia.search_movie(string2)
+    ID = info[0].movieID
+    movie = ia.get_movie(ID)
+    for genre in movie["genres"]:
+        return genre
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
