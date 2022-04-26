c88458d56983d2d1b9b77dce6ec80d7103e3a45d
Himangshu Raj Bhantana <89568663+hb173@users.noreply.github.com>
2022-04-26 13:30:55 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -9,3 +9,8 @@ def test_root():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello folks!"}
 
+def test2():
+    response = client.get("/movie/Harry")
+    assert response.status_code == 200
+    assert response.json() == {'Harry', 'Harry', 'Harry', "Harry Potter and the Sorcerer's Stone", 'Bosch', 'Harry Potter and the Goblet of Fire', 'Harry Potter and the Deathly Hallows: Part 2', 'The Ipcress File', 'Barry', 'Harry Wild', 'Marry Me', 'Harry Potter and the Prisoner of Azkaban', 'Harry Potter and the Chamber of Secrets', 'Harry Potter and the Order of the Phoenix', 'The Ultimatum: Marry or Move On', 'Harry Potter and the Half-Blood Prince', 'Harry Potter and the Deathly Hallows: Part 1', 'Dirty Harry', 'When Harry Met Sally...', 'Harrow'}
+
