f56d75947f5e2bdefda9e805718fc40b9a0e59f5
Himangshu Raj Bhantana <89568663+hb173@users.noreply.github.com>
2022-04-26 15:38:38 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -13,3 +13,34 @@ def test_root():
         "Hello folks! In order to use the recommendation engine, type one of the following:"
         " Sad, Angry, Anticipating, Horror, Content, Motivated, or Bollywood"
     ]
+def test2():
+    response = client.get("/movie/Harry")
+    assert response.status_code == 200
+    assert response.json() == [
+        "Harry",
+        "Harry",
+        "Harry",
+        "Harry Potter and the Sorcerer's Stone",
+        "Bosch",
+        "Harry Potter and the Goblet of Fire",
+        "Harry Potter and the Deathly Hallows: Part 2",
+        "The Ipcress File",
+        "Barry",
+        "Harry Wild",
+        "Marry Me",
+        "Harry Potter and the Prisoner of Azkaban",
+        "Harry Potter and the Chamber of Secrets",
+        "Harry Potter and the Order of the Phoenix",
+        "The Ultimatum: Marry or Move On",
+        "Harry Potter and the Half-Blood Prince",
+        "Harry Potter and the Deathly Hallows: Part 1",
+        "Dirty Harry",
+        "When Harry Met Sally...",
+        "Harrow",
+    ]
+def test3():
+    response = client.get("/genre/lion king")
+    assert response.status_code == 200
+    assert response.json() == [
+        'Animation'
+    ]
