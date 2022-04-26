cf3a6fef659aacf47755fefb40e0f9e98f4cf71c
Himangshu Raj Bhantana <89568663+hb173@users.noreply.github.com>
2022-04-26 15:27:25 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,6 +10,3 @@ def test_root():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello folks!"}
 
-def test2():
-    response = client.get("/movie/string1?string1=lion king')
-    assert response.json() == ['The Lion King', 'Lion King', 'Lion King', 'Untitled Lion King Prequel', 'Lion Kingdom', 'Lion King - Free Healthcare', 'Lion King - Behind the Scenes', 'Lion King: Adventure at Pride Rock', 'The Lion King', 'The Scorpion King', 'The Lion King', "The Lion King II: Simba's Pride", 'The Lion King', 'The Lion King 3: Hakuna Matata', 'The Lion Guard Drama King', 'Simba: The King Lion', 'The Lion King 1½', 'Jason King', 'The Making of the Lion King', 'The Lion King Read-Along']
