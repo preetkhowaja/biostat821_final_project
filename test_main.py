e345e2fda4c6a374be23c19df948e3e9e0aa048f
Preet Khowaja <preet.khowaja@duke.edu>
2022-04-26 15:23:32 -0400

    testing edit2

--- test_main.py
+++ test_main.py
@@ -4,11 +4,11 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {
+    assert response.json() == [
         "Hello folks! In order to use the recommendation engine, type one of the following:"
         " Sad, Angry, Anticipating, Horror, Content, Motivated, or Bollywood"
-    }
-
+    ]
